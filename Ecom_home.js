c7820e8785986f3ddd06ce052cf9798b8cb8de80
enoshjoshuagithub <143170357+enoshjoshuagithub@users.noreply.github.com>
2024-06-25 12:29:21 +0530

    Add files via upload

---
+++ Ecom_home.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+const links = document.getElementById("links");
+
+hamburger.addEventListener("click", ()=>{
+
+    links.classList.toggle("links-active");
+}) 
